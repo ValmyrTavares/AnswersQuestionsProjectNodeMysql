3898ffdcb62038373f2de37cea6c5333eb11ebbc
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-07 12:28:08 -0300

    usando ejs com variaveis e rotas com paramentros

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set("view engine", 'ejs');
 
-app.get("/", (req, res)=>{
-    res.render("index")
+app.get("/:nome/:lang", (req, res)=>{
+    let nome= req.params.nome;
+    let lang=req.params.lang;
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do Programador", 
+        inscrito : 8000
+    })
 })
 
 
