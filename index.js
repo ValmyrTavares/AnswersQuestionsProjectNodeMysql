e41c305741027e4705cb2f770f61ebc9d370893b
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-07 12:37:30 -0300

    Usando estruturas condicionais dentro do ejs

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ app.set("view engine", 'ejs');
 app.get("/:nome/:lang", (req, res)=>{
     let nome= req.params.nome;
     let lang=req.params.lang;
+    let exibirMsg = true;
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Guia do Programador", 
-        inscrito : 8000
+        inscrito : 8000,
+        msg: exibirMsg
     })
 })
 
