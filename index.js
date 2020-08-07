7146dcc42baeb301397dd9e11fbc05146e95ed35
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-08-07 13:02:42 -0300

    Usando forEach dentro ejs

--- index.js
+++ index.js
@@ -7,12 +7,21 @@ app.get("/:nome/:lang", (req, res)=>{
     let nome= req.params.nome;
     let lang=req.params.lang;
     let exibirMsg = true;
+
+    let produtos = [
+        {nome: "Valmyr", idade: 49, nacionalidade: "Frances"},
+        {nome: "Vania", idade: 29, nacionalidade: "Hungara"},
+        {nome: "Michelle", idade: 19, nacionalidade: "Italiana"},
+    ]
+
+
     res.render("index",{
         nome: nome,
         lang: lang,
         empresa: "Guia do Programador", 
         inscrito : 8000,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos:produtos
     })
 })
 
